a5329c45720a605a162678c5bfa6a119d1887ded
Sean <pca.sean.jeun@gmail.com>
2023-07-11 12:27:19 -0400

    card.rb done(?)

--- practice-for-ch-ruby-memory-long-practice-main/card.rb
+++ practice-for-ch-ruby-memory-long-practice-main/card.rb
@@ -1,4 +1,35 @@
 class Card
 
-    
+    attr_reader :face_val, :current_state, :hidden_state
+
+    def initialize(letter)
+        @face_val = letter
+        @hidden_state = true
+        @current_state = @hidden_state
+    end
+
+    def hidden?
+       if @current_state == @hidden_state
+            return true
+       end
+
+       return false
+    end
+
+    def face_up?
+        if @current_state == face_val
+            return true
+        end
+        
+        return false
+    end
+
+    def switch_side!
+        if @current_state == @hidden_state
+            @current_state = @face_val
+        else 
+            @current_state = @hidden_state
+        end
+    end
+
 end
