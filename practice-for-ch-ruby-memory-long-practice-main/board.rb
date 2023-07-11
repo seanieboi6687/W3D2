d2d46b8d014b642c52eb9c8aaf7642c3c1092d2b
Sean <pca.sean.jeun@gmail.com>
2023-07-11 14:24:32 -0400

    board initialize done

--- practice-for-ch-ruby-memory-long-practice-main/board.rb
+++ practice-for-ch-ruby-memory-long-practice-main/board.rb
@@ -1,3 +1,38 @@
+require_relative "card"
+
 class Board
 
+    def initialize
+        @letters = ("A".."H").to_a
+        @cards = []
+        @board = Array.new(4) {Array.new}
+        @size = 16 
+        @letters.each do |char|
+            @cards << Card.new(char)
+            @cards << Card.new(char)
+        end
+
+        @board.each do |row|
+            4.times do 
+                row << @cards.pop
+            end
+        end
+        @cards.shuffle!
+    end 
+
+    def populate
+
+    end
+
+    def render
+
+    end
+
+    def won?
+
+    end
+
+    def reveal
+
+    end
 end
